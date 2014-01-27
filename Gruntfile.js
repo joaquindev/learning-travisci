fa016e7e4cac77534a3b6a5c69123f9f1fe2b96b
JoaquinDev <joaquin@isla.io>
2014-01-27 13:22:10 +0100

    Add .overalls.yml to learn how to use it
    Add .overalls.yml to learn how to use it
    
    Contact: joaquin@isla.io

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,9 +2,21 @@ module.exports = function(grunt){
   grunt.initConfig({
     jshint: {
       all: ['Gruntfile.js', 'hello.js']
+    },
+    coveralls: {
+      options: {
+        // LCOV coverage file relevant to every target
+        src: 'coverage-results/lcov.info'
+      },
+      your_target: {
+        //Target-specific LCOV coverage file
+        src: 'coverage-results/extra-results-*.info'
+      }
     }
+    
   });
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-coveralls');
   
   grunt.registerTask('default', 'jshint');
 };
